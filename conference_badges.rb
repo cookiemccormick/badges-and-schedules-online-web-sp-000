5dd5186f14178cf6e6953c6a7ffbe20f895424a6
Alisa McCormick <alisa.cookie.mccormick@gmail.com>
2018-10-10 19:18:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.each {|name| list << badge_maker(name)
+  end
+end
+
+def assign_rooms(speakers)
+  assignment = []
+  speakers.each_with_index do |speaker, index|
+    assignment << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+  end
+  assignment
+end
